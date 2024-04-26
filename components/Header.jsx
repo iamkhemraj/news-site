d3293c0f87a713aabbb91beed57adac57a87bddf
khemraj <khemraj@rwit.io>
2024-04-26 12:01:47 +0530

    This is header

---
+++ components/Header.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import { Component } from "react";
+import { AiOutlineBell, AiOutlineUser } from "react-icons/ai";
+import { FiSearch } from "react-icons/fi";
+import { GiAstronautHelmet } from "react-icons/gi";
+
+export default function Layout({ children }) {
+   return (
+      <div>
+          HEADER
+      </div>
+   );
+}
