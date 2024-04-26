fa3dcd26f8d0ad3771c29787a225c67653e0b69b
khemraj <khemraj@rwit.io>
2024-04-26 16:40:38 +0530

    add just header

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,8 +7,19 @@ import { GiAstronautHelmet } from "react-icons/gi";
 
 export default function Layout({ children }) {
    return (
-      <div>
-         HEADER
-      </div>
+      <header className="header">
+         <div className="header__section">
+            <div className="header__heading">
+               <h3><GiAstronautHelmet/> Momento</h3> 
+            </div>
+            <div className="header__search__bar">
+               <FiSearch/>
+            </div>
+            <div className="header__userhead">
+               <AiOutlineUser/>
+               <AiOutlineBell/>
+            </div>
+         </div>
+      </header>
    );
 }
